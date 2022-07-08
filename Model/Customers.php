cdebf89d7b67fe6d478f025d8bf24612300dc2cd
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-08 15:11:50 +0200

    Model for customers So I made a model for the customers, extend it from class database and the I made a protected function to get the customers and made a public one to show the customers, in this function I work with a foreach to retrieve all the first and lastnames. This seems to work, now let's see what happens if we put it in a select field

---
+++ Model/Customers.php
@@ -0,0 +1,21 @@
+<?php
+
+class Customers extends Database
+{
+
+    protected function getCustomers() {
+        $sql = "SELECT firstname, lastname FROM customer";
+        $stmt = $this->connect()->query($sql);
+
+        $results = $stmt->fetchAll();
+        return $results;
+    }
+
+    public function showCustomers() {
+        $results = $this->getCustomers();
+        foreach ($results as $result){
+            echo $result['firstname'] . '&nbsp;' . $result['lastname'] . "<br>";
+        }
+    }
+
+}
