75e856869533943f881f1f03493abcf8d5723a4f
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-12 12:28:15 +0200

    added a refactor function got following error, Fatal error: Uncaught TypeError: Cannot assign null to property PriceCalculator:: of type int. So we needed to add a function that will set NULL values to 0

--- Model/PriceCalculator.php
+++ Model/PriceCalculator.php
@@ -14,6 +14,8 @@ class PriceCalculator
     private int $highestVariableDiscount;
     //all the discounts
     private array $allDiscounts;
+    //our calculation result
+    private float $result;
 
     public function __construct(array $customerDetails, array $productDetails, array $groupDiscountDetails){
         $this->customerDetails = $customerDetails;
@@ -23,8 +25,21 @@ class PriceCalculator
         $this->allDiscounts['fixed_discount'] = $this->customerDetails['fixed_discount'];
         $this->allDiscounts['variable_discount'] = $this->customerDetails['variable_discount'];
         //The discounts from the customers group also need to be added to this array
-        $this->allDiscounts['fixed_discount_group'] = $this->groupDiscountDetails['fixed_discount'];
-        $this->allDiscounts['variable_discount_group'] = $this->groupDiscountDetails['variable_discount'];
+        $this->allDiscounts['fixed_discount_group'] = $this->groupDiscountDetails[0]['fixed_discount'];
+        $this->allDiscounts['variable_discount_group'] = $this->groupDiscountDetails[0]['variable_discount'];
+    }
+
+    /*
+     * Need to add a function which sets NULL value to 0 int value else we get the following error
+     * Fatal error: Uncaught TypeError: Cannot assign null to property PriceCalculator::$highestFixedDiscount of type int
+     */
+
+    public function refactorDiscounts(){
+        foreach ($this->allDiscounts as $type => $discount){
+            if ($discount == null){
+                $this->allDiscounts[$type] = 0;
+            }
+        }
     }
 
     public function getHighestFixedDiscount(): int
@@ -36,5 +51,37 @@ class PriceCalculator
         }
         return $this->highestFixedDiscount;
     }
+
+    public function getHighestVariableDiscount(): int
+    {
+        if ($this->allDiscounts['variable_discount'] > $this->allDiscounts['variable_discount_group']) {
+            $this->highestVariableDiscount = $this->allDiscounts['variable_discount'];
+        } else {
+            $this->highestVariableDiscount = $this->allDiscounts['variable_discount_group'];
+        }
+        return $this->highestVariableDiscount;
+    }
+
+    public function priceCalculation(){
+        $productPrice = $this->productDetails['price']/100; //gives a price with a decimal point
+        $fixedDiscountPrice = $productPrice - $this->getHighestFixedDiscount();
+        $variableDiscountPrice = $productPrice - (($productPrice/100) * $this->getHighestVariableDiscount());
+
+        if ($fixedDiscountPrice > $variableDiscountPrice){
+            $this->result = $variableDiscountPrice;
+        } else {
+            $this->result = $fixedDiscountPrice;
+        }
+
+        if ($this->result < 0){
+            $this->result = 0;
+        }
+    }
+
+    public function getResult(): float
+    {
+        return $this->result;
+    }
+
 
 }
