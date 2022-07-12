03b817a3c45f2ea74c32837736c558847ba6025b
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-12 09:20:06 +0200

    changed the variable name
    getting customers and products

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -6,8 +6,12 @@ class HomepageController
     //render function with both $_GET and $_POST vars available if it would be needed.
     public function render(array $GET, array $POST)
     {
-        //this is just example code, you can remove the line below
-        $user = new User('John Smith');
+        $dataSource = new Database();
+        $customerNames = $dataSource->getCustomers();
+        $productNames = $dataSource->getProducts();
+
+
+
 
         // you should not echo anything inside your controller - only assign vars here
         
